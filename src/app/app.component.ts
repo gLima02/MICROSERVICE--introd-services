6c7b61563fdf82a59c737501de3011ae0b0850ee
lima-fiap <logonrmlocal@fiap.com.br>
2024-04-11 22:28:55 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClienteComponent } from './componentes/cliente/cliente.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClienteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
