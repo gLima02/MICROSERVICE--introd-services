e1dcf9ad7ed2e53d2dd3f39a3ef5bc6a809c6101
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-25 20:07:33 -0300

    nova-rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RotaComponent } from './componentes/rota/rota.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'nova-rota', component: RotaComponent},
+  {path: '**', component:HomeComponent}
+];
