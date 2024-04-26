94d95c9f48723d289cda76b5dca3c4d7927ec881
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-25 21:39:57 -0300

    client detail route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RotaComponent } from './componentes/rota/rota.component';
+import { ClientDetailComponent } from './componentes/client-detail/client-detail.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'nova-rota', component: RotaComponent},
+  // tem ligacao com o ClientDetail
+  {path: 'cliente/:id', component: ClientDetailComponent},
   // '**' faz com que qualquer endereco leve ao home
-  {path: '**', component:HomeComponent}
+  // sempre que tem que estar por ultimo
+  {path: '**', component:HomeComponent},
 ];
